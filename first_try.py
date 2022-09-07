916cea6ddd337a810b4a5141d2004b5b7ec08ee9
Artem-Vorobiov <artem@Artems-MacBook-Pro.local>
2022-09-08 03:19:36 +0800

    First

---
+++ first_try.py
@@ -0,0 +1,12 @@
+import numpy
+import sys 
+
+print('\n')
+print(numpy.__version__)
+
+# print(sys.version_info)
+# print(type(sys.version_info))
+
+x = slice(2)
+print(sys.version_info[x])
+print('\n')
